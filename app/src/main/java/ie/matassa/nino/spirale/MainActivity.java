af8806c8eb12135187b824058f8f704df3fcdece
Nino-Matassa <nino.matassa@gmail.com>
2020-12-10 04:59:53 +0000

    Initial commit

---
+++ app/src/main/java/ie/matassa/nino/spirale/MainActivity.java
@@ -0,0 +1,14 @@
+package ie.matassa.nino.spirale;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
