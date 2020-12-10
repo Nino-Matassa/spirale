6fc288e8540db0a85c176516b64bf49873d45b1f
Nino-Matassa <nino.matassa@gmail.com>
2020-12-10 07:09:09 +0000

    Initial constant information added

--- app/src/main/java/ie/matassa/nino/spirale/MainActivity.java
+++ app/src/main/java/ie/matassa/nino/spirale/MainActivity.java
@@ -11,4 +11,40 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+	@Override
+	protected void onStart() {
+		// TODO: Implement this method
+		super.onStart();
+	}
+
+	@Override
+	protected void onRestart() {
+		// TODO: Implement this method
+		super.onRestart();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO: Implement this method
+		super.onResume();
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		// TODO: Implement this method
+		super.onRestoreInstanceState(savedInstanceState);
+	}
+
+	@Override
+	public void onRestoreInstanceState(Bundle savedInstanceState, PersistableBundle persistentState) {
+		// TODO: Implement this method
+		super.onRestoreInstanceState(savedInstanceState, persistentState);
+	}
+
+	@Override
+	public void onBackPressed() {
+		// TODO: Implement this method
+		super.onBackPressed();
+	}
 }
