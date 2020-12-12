dffa1ea767718ae9e881fa3f39c42b5547030237
Nino-Matassa <nino.matassa@gmail.com>
2020-12-12 09:05:52 +0000

    Add com.android.support:design:23.3.0 to build

--- app/src/main/java/ie/matassa/nino/spirale/MainActivity.java
+++ app/src/main/java/ie/matassa/nino/spirale/MainActivity.java
@@ -2,9 +2,27 @@ package ie.matassa.nino.spirale;
 
 import android.app.*;
 import android.os.*;
+import android.support.v7.app.*;
+import android.content.*;
 
-public class MainActivity extends Activity 
-{
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
+
+	@Override
+	public Loader<String> onCreateLoader(int p1, Bundle p2) {
+		// TODO: Implement this method
+		return null;
+	}
+
+	@Override
+	public void onLoadFinished(Loader<String> p1, String p2) {
+		// TODO: Implement this method
+	}
+
+	@Override
+	public void onLoaderReset(Loader<String> p1) {
+		// TODO: Implement this method
+	}
+	
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
