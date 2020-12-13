ad7c6118a408f5663357832dd416a9cf3c23df62
Nino-Matassa <nino.matassa@gmail.com>
2020-12-13 09:06:16 +0000

    CSV download function added

--- app/src/main/java/ie/matassa/nino/spirale/Constants.java
+++ app/src/main/java/ie/matassa/nino/spirale/Constants.java
@@ -3,8 +3,10 @@ package ie.matassa.nino.spirale;
 public class Constants
 {
 	public static final String DataSource = "WHO (https://covid19.who.int/info)";
-	public static final String MainDatabaseURL = "https://covid19.who.int/WHO-COVID-19-global-data.csv";
-	public static final String OverviewDatabaseURL = "https://covid19.who.int/WHO-COVID-19-global-table-data.csv";
-	public static final String dbName = "dbSpirale";
+	public static final String CsvDetailsURL = "https://covid19.who.int/WHO-COVID-19-global-data.csv";
+	public static final String CsvOverviewURL = "https://covid19.who.int/WHO-COVID-19-global-table-data.csv";
+	public static final String dbName = null; // "dbSpirale"; // null = keep it in memory
+	public static final String csvDetailsName = "DetailsTable.csv";
+	public static final String csvOverviewName = "OverviewTable.csv";
 }
 
--- app/src/main/java/ie/matassa/nino/spirale/MainActivity.java
+++ app/src/main/java/ie/matassa/nino/spirale/MainActivity.java
@@ -1,16 +1,56 @@
 package ie.matassa.nino.spirale;
 
 import android.app.*;
+import android.content.*;
 import android.os.*;
 import android.support.v7.app.*;
-import android.content.*;
+import android.util.*;
+import java.io.*;
+import java.net.*;
+import java.nio.channels.*;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
 
 	@Override
-	public Loader<String> onCreateLoader(int p1, Bundle p2) {
-		// TODO: Implement this method
-		return null;
+	public Loader<String> onCreateLoader(int id, Bundle urls) {
+		
+		return new AsyncTaskLoader<String>(this) {
+			private boolean downloadMainDB = false;
+			private boolean downloadOverviewDB = false;
+			
+			private String resultString = "";
+
+			@Override
+			public String loadInBackground() {
+				
+				return resultString;
+			}
+			@Override
+			protected void onStartLoading(){
+				// progress bar, etc.
+				forceLoad();
+			}
+			
+			private boolean downloadCSV(String csvFileName, String csvUrl) {
+				String filePath = MainActivity.this.getFilesDir().getPath().toString() + "/" + csvFileName;
+				File file = new File(filePath);
+				if(file.exists()) file.delete();
+
+				try {
+					ReadableByteChannel readChannel = Channels.newChannel(new URL(csvUrl).openStream());
+					FileOutputStream fileOS = new FileOutputStream(filePath);
+					FileChannel writeChannel = fileOS.getChannel();
+					writeChannel.transferFrom(readChannel, 0, Long.MAX_VALUE);
+					writeChannel.close();
+					readChannel.close();
+				}
+				catch(IOException e) {
+					Log.d("downloadCSV", e.toString());
+					return false;
+				}
+				return true;
+			}
+		};
 	}
 
 	@Override
