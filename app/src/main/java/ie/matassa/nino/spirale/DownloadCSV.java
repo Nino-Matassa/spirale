470735a864784e96ab44b57de1b93eda538f3552
Nino-Matassa <nino.matassa@gmail.com>
2020-12-14 12:03:33 +0000

    CSV download setup

---
+++ app/src/main/java/ie/matassa/nino/spirale/DownloadCSV.java
@@ -0,0 +1,12 @@
+package ie.matassa.nino.spirale;
+import android.content.*;
+
+public class DownloadCSV {
+	
+	Context context = null;
+	String Url = null;
+	public DownloadCSV(Context context, String Url) {
+		this.context = context;
+		this.Url = Url;
+	}
+}
--- app/src/main/java/ie/matassa/nino/spirale/MainActivity.java
+++ app/src/main/java/ie/matassa/nino/spirale/MainActivity.java
@@ -1,82 +1,16 @@
 package ie.matassa.nino.spirale;
 
 import android.app.*;
-import android.content.*;
 import android.os.*;
-import android.support.v7.app.*;
-import android.util.*;
-import java.io.*;
-import java.net.*;
-import java.nio.channels.*;
 
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
+public class MainActivity extends Activity {
 
 	@Override
-	public Loader<String> onCreateLoader(int id, Bundle urls) {
-		
-		return new AsyncTaskLoader<String>(this) {
-			private boolean downloadMainDB = false;
-			private boolean downloadOverviewDB = false;
-			
-			private String resultString = "";
-
-			@Override
-			public String loadInBackground() {
-				
-				return resultString;
-			}
-			@Override
-			protected void onStartLoading(){
-				// progress bar, etc.
-				forceLoad();
-			}
-			
-			private boolean downloadCSV(String csvFileName, String csvUrl) {
-				String filePath = MainActivity.this.getFilesDir().getPath().toString() + "/" + csvFileName;
-				File file = new File(filePath);
-				if(file.exists()) file.delete();
-
-				try {
-					ReadableByteChannel readChannel = Channels.newChannel(new URL(csvUrl).openStream());
-					FileOutputStream fileOS = new FileOutputStream(filePath);
-					FileChannel writeChannel = fileOS.getChannel();
-					writeChannel.transferFrom(readChannel, 0, Long.MAX_VALUE);
-					writeChannel.close();
-					readChannel.close();
-				}
-				catch(IOException e) {
-					Log.d("downloadCSV", e.toString());
-					return false;
-				}
-				return true;
-			}
-		};
-	}
-
-	@Override
-	public void onLoadFinished(Loader<String> p1, String p2) {
+	public void onCreate(Bundle savedInstanceState) {
 		// TODO: Implement this method
-	}
-
-	@Override
-	public void onLoaderReset(Loader<String> p1) {
-		// TODO: Implement this method
-	}
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
-	@Override
-	protected void onResume() {
-		// TODO: Implement this method
-		super.onResume();
-	}
-	@Override
-	public void onBackPressed() {
-		// TODO: Implement this method
-		super.onBackPressed();
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 	}
 }
+
+
