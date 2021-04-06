369e14bf3243ab7ed8baad07f6195927c4eb678d
Nino Matassa <nino.matassa@gmail.com>
2021-04-06 05:12:31 +0100

    UIRHSTerraRNought corrections, use identity where no cases have been reported.

--- app/src/main/java/ie/matassa/nino/spirale/RNoughtCalculation.java
+++ app/src/main/java/ie/matassa/nino/spirale/RNoughtCalculation.java
@@ -7,60 +7,58 @@ import android.util.*;
 
 public class RNoughtCalculation {
 
-  public RNoughtCalculation() {}
+	public RNoughtCalculation() {}
 
-  private Double calculate(int current, int previous) {
-	Double rNought = 0.0;
+	private Double calculate(int current, int previous) {
+		Double rNought = 0.0;
 
-	if (current > 0 && previous > 0) {
-	  rNought = current / (double)previous;
-	} else if (current > 0 && previous == 0) {
-	  current = 1;
-	} else {
-	  rNought = 0.0;
-	} 
+		// Use identity where zero cases are reported
+		if (current == 0) current = 1;
+		if (previous == 0) previous = 1;
 
-	return rNought;
-  }
-
-  public ArrayList<RNoughtAverage> calculate(Cursor cursor, int length) {
-	ArrayList<RNoughtAverage> listRNought = new ArrayList<RNoughtAverage>();
-
-	// Populate listRNought
-	cursor.moveToFirst();
-	do {
-	  RNoughtAverage value = new RNoughtAverage();
-	  String date = cursor.getString(cursor.getColumnIndex("Date"));
-	  try {
-		date = new SimpleDateFormat("yyyy-MM-dd").parse(date).toString();
-		String[] arrDate = date.split(" ");
-		date = arrDate[0] + " " + arrDate[1] + " " + arrDate[2] + " " + arrDate[5];
-	  } catch (Exception e) {
-		Log.d(Constants.UICase24Hour, e.toString());
-	  }
-	  value.date = date;
-	  value.newCase = cursor.getInt(cursor.getColumnIndex("CaseX"));
-	  listRNought.add(value);
-	} while(cursor.moveToNext());
-
-	// Populate listRNought rNought values
-	for (int outer = 0; outer < listRNought.size() - 1; outer++) {
-	  int current = listRNought.get(outer).newCase;
-	  int previous = listRNought.get(outer + 1).newCase;
-	  listRNought.get(outer).rNought = calculate(current, previous);
+		rNought = current / (double)previous;
+		
+		return rNought;
 	}
 
-	// Populate listRNought average values
-	for (int outer = 0; outer < listRNought.size() - 1; outer++) {
-	  double rNought = 0.0;
-	  for (int inner = 0; inner < length; inner++) {
-		if (outer + length > listRNought.size())
-		  break;
-		rNought += listRNought.get(outer + inner).rNought;
-	  }
-	  listRNought.get(outer).average = rNought / (length + 1);
+	public ArrayList<RNoughtAverage> calculate(Cursor cursor, int length) {
+		ArrayList<RNoughtAverage> listRNought = new ArrayList<RNoughtAverage>();
+
+		// Populate listRNought
+		cursor.moveToFirst();
+		do {
+			RNoughtAverage value = new RNoughtAverage();
+			String date = cursor.getString(cursor.getColumnIndex("Date"));
+			try {
+				date = new SimpleDateFormat("yyyy-MM-dd").parse(date).toString();
+				String[] arrDate = date.split(" ");
+				date = arrDate[0] + " " + arrDate[1] + " " + arrDate[2] + " " + arrDate[5];
+			} catch (Exception e) {
+				Log.d(Constants.UICase24Hour, e.toString());
+			}
+			value.date = date;
+			value.newCase = cursor.getInt(cursor.getColumnIndex("CaseX"));
+			listRNought.add(value);
+		} while(cursor.moveToNext());
+
+		// Populate listRNought rNought values
+		for (int outer = 0; outer < listRNought.size() - 1; outer++) {
+			int current = listRNought.get(outer).newCase;
+			int previous = listRNought.get(outer + 1).newCase;
+			listRNought.get(outer).rNought = calculate(current, previous);
+		}
+
+		// Populate listRNought average values
+		for (int outer = 0; outer < listRNought.size() - 1; outer++) {
+			double rNought = 0.0;
+			for (int inner = 0; inner < length; inner++) {
+				if (outer + length > listRNought.size())
+					break;
+				rNought += listRNought.get(outer + inner).rNought;
+			}
+			listRNought.get(outer).average = rNought / (length + 1);
+		}
+		return listRNought;
 	}
-	return listRNought;
-  }
 }
 
